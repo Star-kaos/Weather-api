fadab5ff9bc17a436105ff600d8f1bbd2505dca2
Star_kaos <perera.elijah@gmail.com>
2022-07-01 15:39:51 -0700

    Completed UI and the backend for project

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("Ready to go!")
+
+//request to site
+function loadRequest(weatherUrl) {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = () => {
+        if (request.readyState === 4 && request.status === 200) {
+            let weatherData = JSON.parse(request.response)
+            //let temperature = weatherData.main.temp;
+            displayTemp(weatherData)
+        }
+    }
+    request.open('GET', weatherUrl)
+    request.send();
+}
+
+//on submit button clicked
+document.getElementById("lat-lonSubmit").addEventListener("click", () => {
+    let lat = document.getElementById("latInput").value;
+    let lon = document.getElementById("lonInput").value;
+
+    let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0c3e0d4bdcbfe0078f4447929dd81bff&units=imperial`;
+
+    //starting call request
+    loadRequest(weatherUrl)
+})
+
+//display temp
+function displayTemp(weatherData) {
+    console.log(weatherData.main.temp)
+    document.getElementById("requestH").innerHTML = `Your temp is: ${weatherData.main.temp} °f`;
+}
